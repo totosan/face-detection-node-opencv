37dd2aca7ce5717890b73af901249b1f6c035019
toto_san@live.com <toto_san@live.com>
2015-04-09 08:05:14 +0200

    added solution and so on

---
+++ config.js
@@ -0,0 +1,11 @@
+﻿//this file holds configuration variables
+//you can add any that you may need for your application
+
+module.exports = {
+    host: '192.168.1.12',
+    user: 'root',
+    password: 'QxT638_!',
+    projectName: 'FaceDetection',
+    startFile: 'server/server.js',
+    sshPort: 22
+}
